ff6ed34b850fbd304052404710ef69a6b48e25a7
Maria V <mariatua2@gmail.com>
2022-02-16 13:47:29 -0400

    creating nfts from frontend

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -8,89 +8,80 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract NFTMarket is ReentrancyGuard {
-    using Counters for Counters.Counter;
-    // two counters are going to be for the items ID
-    //working with solidity you need the link of the arrays, that's why we use counters
-    Counters.Counter private _itemIds;
-    Counters.Counter private _itemsSold;
-    address payable owner; // the owner makes a commision for every item sold
-    uint256 listingPrice = 0.025 ether; //npi que esta haciendo
+  using Counters for Counters.Counter;
+  Counters.Counter private _itemIds;
+  Counters.Counter private _itemsSold;
 
-    constructor() {
-        owner = payable(msg.sender); // basically saying "the owner of this contract is the person deploying it"
-    }
+  address payable owner;
+  uint256 listingPrice = 0.025 ether;
 
+  constructor() {
+    owner = payable(msg.sender);
+  }
 
-    //define struct for each individual market item
-    struct MarketItem {
-        uint itemId;
-        address nftContract;
-        uint256 tokenId;
-        address payable seller;
-        address payable owner;
-        uint256 price;
-        bool sold;
-    }
+  struct MarketItem {
+    uint itemId;
+    address nftContract;
+    uint256 tokenId;
+    address payable seller;
+    address payable owner;
+    uint256 price;
+    bool sold;
+  }
 
-    mapping(uint256 => MarketItem) private idToMarketItem; //this mapping allow us to find a market item with the ID
+  mapping(uint256 => MarketItem) private idToMarketItem;
 
-    // event for when the market item is created
-    event MarketItemCreated (
-        uint indexed itemId,
-        address indexed nftContract,
-        uint256 indexed tokenId,
-        address seller,
-        address owner,
-        uint256 price,
-        bool sold
+  event MarketItemCreated (
+    uint indexed itemId,
+    address indexed nftContract,
+    uint256 indexed tokenId,
+    address seller,
+    address owner,
+    uint256 price,
+    bool sold
+  );
+
+  /* Returns the listing price of the contract */
+  function getListingPrice() public view returns (uint256) {
+    return listingPrice;
+  }
+  
+  /* Places an item for sale on the marketplace */
+  function createMarketItem(
+    address nftContract,
+    uint256 tokenId,
+    uint256 price
+  ) public payable nonReentrant {
+    require(price > 0, "Price must be at least 1 wei");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
+
+    _itemIds.increment();
+    uint256 itemId = _itemIds.current();
+  
+    idToMarketItem[itemId] =  MarketItem(
+      itemId,
+      nftContract,
+      tokenId,
+      payable(msg.sender),
+      payable(address(0)),
+      price,
+      false
     );
 
-    /* Returns the listing price of the contract */
-    function getListingPrice() public view returns (uint256) {
-        return listingPrice;
-    }
+    IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
 
-    function createMarketItem(
-        address nftContract, //contract address for this NFT
-        uint256 tokenId,
-        uint256 price
-    ) public payable nonReentrant { // prevent a reentry
-        //conditions
-        require(price > 0, "Price must be at least 1 wei"); //price greater than 0
-        require(msg.value == listingPrice, "Price must be equal to listing price"); //require the person pays the listing price as well as the transaction
+    emit MarketItemCreated(
+      itemId,
+      nftContract,
+      tokenId,
+      msg.sender,
+      address(0),
+      price,
+      false
+    );
+  }
 
-        _itemIds.increment();
-        uint256 itemId = _itemIds.current();
-
-        //mapping creating market item
-        idToMarketItem[itemId] =  MarketItem(
-            itemId,
-            nftContract,
-            tokenId,
-            payable(msg.sender),
-            payable(address(0)), //empty address in the seller, as to keeping it with no value
-            price,
-            false
-        );
-
-        //transfer ownership from sender to the contract, so the contract can transfer it to the buyer
-        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
-
-        //then we emmit the event
-
-        emit MarketItemCreated(
-            itemId,
-            nftContract,
-            tokenId,
-            msg.sender,
-            address(0),
-            price,
-            false
-        );
-
-    }
-
-      /* Creates the sale of a marketplace item */
+  /* Creates the sale of a marketplace item */
   /* Transfers ownership of the item, as well as funds between parties */
   function createMarketSale(
     address nftContract,
@@ -105,7 +96,7 @@ contract NFTMarket is ReentrancyGuard {
     idToMarketItem[itemId].owner = payable(msg.sender);
     idToMarketItem[itemId].sold = true;
     _itemsSold.increment();
-    payable(owner).transfer(listingPrice); //pay the owner of the contract, the commission
+    payable(owner).transfer(listingPrice);
   }
 
   /* Returns all unsold market items */
@@ -115,9 +106,8 @@ contract NFTMarket is ReentrancyGuard {
     uint currentIndex = 0;
 
     MarketItem[] memory items = new MarketItem[](unsoldItemCount);
-    //basically we look for all the NFT not sold and add it to the items array
     for (uint i = 0; i < itemCount; i++) {
-      if (idToMarketItem[i + 1].owner == address(0)) { //we know it is not sold because the owner is address(0) (empty)
+      if (idToMarketItem[i + 1].owner == address(0)) {
         uint currentId = i + 1;
         MarketItem storage currentItem = idToMarketItem[currentId];
         items[currentIndex] = currentItem;
@@ -127,21 +117,18 @@ contract NFTMarket is ReentrancyGuard {
     return items;
   }
 
-  /* Returns only that items that a user has purchased */
+  /* Returns onlyl items that a user has purchased */
   function fetchMyNFTs() public view returns (MarketItem[] memory) {
     uint totalItemCount = _itemIds.current();
     uint itemCount = 0;
     uint currentIndex = 0;
 
-
-    // here I'm looping through all items and seeing how many of them I own
     for (uint i = 0; i < totalItemCount; i++) {
       if (idToMarketItem[i + 1].owner == msg.sender) {
         itemCount += 1;
       }
     }
 
-    //basically we look for all that are mine and add it to the items array
     MarketItem[] memory items = new MarketItem[](itemCount);
     for (uint i = 0; i < totalItemCount; i++) {
       if (idToMarketItem[i + 1].owner == msg.sender) {
@@ -160,7 +147,6 @@ contract NFTMarket is ReentrancyGuard {
     uint itemCount = 0;
     uint currentIndex = 0;
 
-    // same as last function but we are looking for the msg.sender
     for (uint i = 0; i < totalItemCount; i++) {
       if (idToMarketItem[i + 1].seller == msg.sender) {
         itemCount += 1;
@@ -178,6 +164,4 @@ contract NFTMarket is ReentrancyGuard {
     }
     return items;
   }
-
-
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,25 @@
 const hre = require("hardhat");
+const fs = require('fs');
 
 async function main() {
-  // deploy NFT market
   const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
   const nftMarket = await NFTMarket.deploy();
   await nftMarket.deployed();
   console.log("nftMarket deployed to:", nftMarket.address);
 
-  // deploy NFT
   const NFT = await hre.ethers.getContractFactory("NFT");
   const nft = await NFT.deploy(nftMarket.address);
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
+
+  let config = `
+  export const nftmarketaddress = "${nftMarket.address}"
+  export const nftaddress = "${nft.address}"
+  `
+
+  let data = JSON.stringify(config)
+  fs.writeFileSync('config.js', JSON.parse(data))
+
 }
 
 main()
@@ -20,7 +28,6 @@ main()
     console.error(error);
     process.exit(1);
   });
-
 
 
 // // We require the Hardhat Runtime Environment explicitly here. This is optional
